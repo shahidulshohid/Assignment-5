c82e7368e9b7a427117e53ed4637fa0fe1f52f6d
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-23 18:25:49 +0600

    history section functionality is done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 
-// show the section by donation and history button
+// show the section by history button
 document.getElementById('history-btn').addEventListener('click', function(){
 
     showSectionById('history-section')
@@ -10,7 +10,7 @@ document.getElementById('history-btn').addEventListener('click', function(){
     document.getElementById('donation-btn').classList.remove('bg-[#B4F461]');
 }); 
 
-
+// show the section by donation button
 document.getElementById('donation-btn').addEventListener('click', function(){
 
     document.getElementById('donation-btn').classList.add('bg-[#B4F461]')
@@ -42,5 +42,20 @@ document.getElementById('noakhali-btn').addEventListener('click', function(){
     let mainBalanceNumber = parseFloat(mainBalance);
     mainBalanceNumber -= inputValue;
     document.getElementById('main-balance').innerText = mainBalanceNumber;
-    
+
+    // history section update 
+    const historyContainer = document.getElementById('history-section')
+    const heading = document.getElementById('heading1').innerText;
+    const div = document.createElement('div');
+    div.classList.add('border-2', 'border-gray-100', 'p-6', 'rounded-xl',)
+    div.innerHTML = `
+    <h4 class='text-xl font-bold my-3'>${noakhaliBalanceNumber} Taka is ${heading}</h4>
+    <p> Date :${new Date()} </p>
+    `
+    historyContainer.appendChild(div);
 })
+
+// All section button handle 
+// function buttonHandle(e){
+// }
+// ata index.html a kora ase somadhan korte hobe
--- js/utilities.js
+++ js/utilities.js
@@ -16,3 +16,4 @@ function getTextFieldValueById(id){
     return inputValueNumber;
 }
 
+
